8b35d1ed70d823d6e66cd8198f6f1e40858abdec
moral2 <moralmado2@gmail.com>
2020-01-11 16:32:35 +0200

    updates

---
+++ cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/FileCacheManager.cpp" "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/FileCacheManager.cpp.o"
+  "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/MySerialServer.cpp" "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/MySerialServer.cpp.o"
+  "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/MyTestClientHandler.cpp" "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/MyTestClientHandler.cpp.o"
+  "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/Solver.cpp" "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/Solver.cpp.o"
+  "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/StringReverser.cpp" "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/StringReverser.cpp.o"
+  "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/test.cpp" "/home/mor/CLionProjects/Generalized-Server-Side-Implementation/cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Generalized-Server-Side-Implementation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Generalized-Server-Side-Implementation.dir/FileCacheManager.cpp.o"
+  "CMakeFiles/Generalized-Server-Side-Implementation.dir/MySerialServer.cpp.o"
+  "CMakeFiles/Generalized-Server-Side-Implementation.dir/MyTestClientHandler.cpp.o"
+  "CMakeFiles/Generalized-Server-Side-Implementation.dir/Solver.cpp.o"
+  "CMakeFiles/Generalized-Server-Side-Implementation.dir/StringReverser.cpp.o"
+  "CMakeFiles/Generalized-Server-Side-Implementation.dir/test.cpp.o"
+  "Generalized-Server-Side-Implementation"
+  "Generalized-Server-Side-Implementation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Generalized-Server-Side-Implementation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
