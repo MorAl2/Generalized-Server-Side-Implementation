e1d3bd5ab97ad3f61397f04eecc7beae531c0b21
moral2 <moralmado2@gmail.com>
2020-01-11 13:54:43 +0200

    updates

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "MySerialServer.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    printf("%s\n",argv[1]);
+    MySerialServer k()
+}
+
+
